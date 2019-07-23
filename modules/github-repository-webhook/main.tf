c128e4f7c1a0d4e4fc19c18b513f273166ba9401
Charlie Chen <charliechen@exosite.com>
2019-07-23 14:51:53 +0800

    Fixate github provider version to ~>1.3.0

--- modules/github-repository-webhook/main.tf
+++ modules/github-repository-webhook/main.tf
@@ -1,6 +1,7 @@
 provider "github" {
   token        = "${var.github_token}"
   organization = "${var.github_organization}"
+  version      = "~> 1.3.0"
 }
 
 resource "github_repository_webhook" "this" {
